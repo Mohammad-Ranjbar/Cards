845e267bd74f5d9949c039859ed1d07c1e17261c
MohammadJavad <mj.ranjbar.94@gmail.com>
2019-05-02 05:46:01 -0400

    Basic Authentication

--- routes/web.php
+++ routes/web.php
@@ -9,29 +9,10 @@ use Illuminate\Support\Facades\Request;
 
 Route::get('admin',function(){
 
-  return  <<<HTML
-
-<!doctype html>
-<html lang="en">
-<head>
-<meta charset="UTF-8">
-             <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-                         <meta http-equiv="X-UA-Compatible" content="ie=edge">
-             <title>Document</title>
-</head>
-<body>
-  <h1>Admin</h1>
-</body>
-</html>
+return 'admin';
 
 
-HTML;
-
-
-
-
-
-});
+})->middleware('auth.basic');
 Route::group(['middlewere'=>['web']],function(){
 
 
